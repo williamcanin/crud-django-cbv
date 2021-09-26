453ea2291efa5643e37b98a9fa18cf9e7a4fe75e
William Canin <william.costa.canin@gmail.com>
2021-09-25 23:55:36 -0300

    update

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
+from .views import SignUpView
+
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='authentication/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .forms import UserRegisterForm
+from django.views.generic.edit import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    template_name = 'authentication/register.html'
+    success_url = reverse_lazy('login')
+    form_class = UserRegisterForm
+    success_message = "Your profile was created successfully"
