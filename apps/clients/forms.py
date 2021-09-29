a3dd9910d081b8f8b45563c43c75d50079e8f936
William Canin <william.costa.canin@gmail.com>
2021-09-29 17:49:36 -0300

    update

--- apps/clients/forms.py
+++ apps/clients/forms.py
@@ -3,13 +3,28 @@ from .models import ClientModel
 
 
 class ClientForm(forms.ModelForm):
-
     class Meta:
         model = ClientModel
-        fields = ("name", "email", "birth_date", "district", "state", "cpf_or_cnpj", "cpf_cnpj", "address", "photo",
-                  "rg", "cep", "cell_phone", "phone", "city")
+        fields = (
+            "name",
+            "email",
+            "birth_date",
+            "district",
+            "state",
+            "cpf_or_cnpj",
+            "cpf_cnpj",
+            "address",
+            "photo",
+            "rg",
+            "cep",
+            "cell_phone",
+            "phone",
+            "city",
+        )
 
         widgets = {
-            'state': forms.Select(attrs={'class': 'form-select'}),
-            'cpf_or_cnpj': forms.Select(attrs={'class': 'form-select clients-form__cpf_or_cnpj'})
+            "state": forms.Select(attrs={"class": "form-select"}),
+            "cpf_or_cnpj": forms.Select(
+                attrs={"class": "form-select clients-form__cpf_or_cnpj"}
+            ),
         }
