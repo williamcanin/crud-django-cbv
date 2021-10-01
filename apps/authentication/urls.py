fcb926ca821c2fa83abcf8972fc08b12642ead79
William C. Canin <william.costa.canin@gmail.com>
2021-09-30 23:26:43 -0300

    Auto commit - D:30092021-H:232643

--- apps/authentication/urls.py
+++ apps/authentication/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView
-from .views import SignUpView
+from .views import SignInView, SignUpView
 
 
 urlpatterns = [
-    path('sign-in/', LoginView.as_view(template_name='authentication/login.html'), name='sign-in'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('sign-in/', SignInView.as_view(template_name='authentication/login.html'), name='sign-in'),
     path('sign-up/', SignUpView.as_view(), name='sign-up'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     # path('password/', PasswordChangeView.as_view(template_name='authentication/password_change.html'), name='password_change'),
 ]
--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -1,7 +1,13 @@
 from .forms import UserRegisterForm
 from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView, PasswordChangeView
 from django.contrib.messages.views import SuccessMessageMixin
+# from django.contrib.auth.models import Group, User
 from django.urls import reverse_lazy
+
+
+class SignInView(LoginView):
+    redirect_authenticated_user = True
 
 
 class SignUpView(SuccessMessageMixin, CreateView):
@@ -9,3 +15,27 @@ class SignUpView(SuccessMessageMixin, CreateView):
     success_url = reverse_lazy('sign-in')
     form_class = UserRegisterForm
     success_message = "Your profile was created successfully"
+
+
+    # def form_valid(self, form):
+    #     form.instance.created_by = self.request.user
+
+    #     get_user = User.objects.get(username='wcanin1')
+    #     read_only = Group.objects.get(name='read_only')
+    #     read_only.user_set.add(get_user)
+
+    #     return super().form_valid(form)
+
+    # def post(self, request, *args, **kwargs):
+    #     username_ = self.request.POST.get("username")
+    #     get_user = User.objects.get(username=username_)
+    #     read_only = Group.objects.get(name='read_only')
+    #     print(username_, get_user, read_only)
+    #     get_user.groups.add(name='read_only')
+    #     # read_only.user_set.add(name=username_)
+
+    #     # read_only = Group.objects.get(name='read_only')
+    #     # read_only.user_set.add(username_)
+
+    #     # self.object = None
+    #     return super().post(request, *args, **kwargs)
