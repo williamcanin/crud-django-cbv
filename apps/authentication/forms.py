02e327df9029b5ce16bc48e98c14214e0acd20ff
William C. Canin <william.costa.canin@gmail.com>
2021-10-01 13:32:25 -0300

    Auto commit - D:01102021-H:133225

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -1,6 +1,7 @@
 from django import forms
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.forms import UserCreationForm
+from contextlib import suppress
 
 
 class UserRegisterForm(UserCreationForm):
@@ -8,4 +9,18 @@ class UserRegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name']
+        fields = ['username', 'email', 'first_name', 'last_name']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+
+        if commit:
+            user.save()
+
+            # Add user t group "read_only"
+            with suppress(Exception):
+                read_only = Group.objects.get(name='read_only')
+                user.groups.add(read_only)
+
+        return user
