92ffded4f8ee9de14d16f5a585c1cabf65c108e5
William Canin <william.costa.canin@gmail.com>
2021-10-03 23:11:07 -0300

    update

--- management/config/settings.py
+++ management/config/settings.py
@@ -140,12 +140,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Add other folder static files
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    os.path.join(BASE_DIR, 'staticfiles/media'),
 ]
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/staticfiles/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles/media')
 
 PHOTO_ENABLE = True if DEBUG else False
 
