c07bc1cf116153429efe9aa046db069faec9460b
William C. Canin <william.costa.canin@gmail.com>
2021-10-10 18:56:51 -0300

    Auto commit - D:10102021-H:185651

--- apps/clients/tests/test_models.py
+++ apps/clients/tests/test_models.py
@@ -3,5 +3,5 @@ from apps.clients.models import ClientModel
 
 def test_clientmodel_str():
 
-    client1 = ClientModel(name="jesus")
-    assert client1.__str__() == "jesus".title()
+    savior = ClientModel(name="jesus")
+    assert savior.__str__() == "jesus".title()
