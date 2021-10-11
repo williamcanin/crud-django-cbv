b3c9663aa8d06132cad6b778549ac8534c5047a8
William C. Canin <william.costa.canin@gmail.com>
2021-10-10 22:49:26 -0300

    Auto commit - D:10102021-H:224926

--- apps/home/tests/test_views.py
+++ apps/home/tests/test_views.py
@@ -8,6 +8,4 @@ def test_template_name_HomeView():
     request = RequestFactory().get("/")
     view = HomeView()
     view.setup(request)
-
-    get_template_name = view.template_name
-    assert get_template_name == "home/home.html"
+    assert view.template_name == "home/home.html"
