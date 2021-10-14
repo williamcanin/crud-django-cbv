3080686c0fc164fd3c5bf659640a28814cec34f5
William C. Canin <william.costa.canin@gmail.com>
2021-10-14 13:43:04 -0300

    Auto commit - D:14102021-H:134304

--- management/config/settings.py
+++ management/config/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 import decouple
 import django_heroku
-from dj_database_url import parse as dburl
+# from dj_database_url import parse as dburl
 from pathlib import Path
 
 
@@ -150,8 +150,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
