3f0ab4df21226b82b08c910243a51da61dabb981
William C. Canin <william.costa.canin@gmail.com>
2021-10-16 21:03:32 -0300

    Auto commit - D:16102021-H:210332

--- apps/clients/templatetags/client_tags.py
+++ apps/clients/templatetags/client_tags.py
@@ -6,3 +6,12 @@ register = template.Library()
 @register.simple_tag
 def buttons_action():
     return ["Editar", "Delelar", "Adicionar", "Cancelar", "Salvar"]
+
+
+@register.simple_tag
+def clients_legend():
+    return {
+        "title": "Legendas",
+        "cpf": "Pessoas Físicas (CPF)",
+        "cnpj": "Pessoas Jurídicas (CNPJ)",
+    }
