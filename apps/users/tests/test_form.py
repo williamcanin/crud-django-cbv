d766555acf0987be22a1547e5d31efa950b6d74e
William C. Canin <william.costa.canin@gmail.com>
2021-10-19 17:18:05 -0300

    Auto commit - D:19102021-H:171805

--- apps/users/tests/test_form.py
+++ apps/users/tests/test_form.py
@@ -1,37 +1,35 @@
-# import pytest
-# from django.urls import reverse_lazy
-# from django.test import Client
-# from django.contrib.auth.models import User
+import pytest
+from django.urls import reverse_lazy
+from django.test import Client
+from django.contrib.auth.models import User
 # from apps.users.forms import UserRegisterForm
+# from apps.users.forms import UserCreationForm
+from apps.users.models import UserCustom, UserManagerCustom
 
-# # from apps.users.views import SignUpView
+# from apps.users.views import SignUpView
+
+@pytest.mark.django_db
+def test_user_not_auth():
+    assert Client().get(reverse_lazy("client_read")).status_code == 302
+    assert Client().get(reverse_lazy("client_new")).status_code == 302
 
 
-# def test_user_not_auth():
-#     assert Client().get(reverse_lazy("client_read")).status_code == 302
-#     assert Client().get(reverse_lazy("client_new")).status_code == 302
+@pytest.mark.django_db
+def test_auth_user():
+    data = {
+        "username": "william.costa.canin@gmail.com",
+        "password1": "!Test123",
+        "password2": "!Test123",
+    }
+    user = UserCustom.objects.create_user(email=data["username"], password=data["password1"])
+    user.save()
 
-
-# @pytest.mark.django_db
-# def test_auth_user():
-#     data = {
-#         "username": "william.costa.canin@gmail.com",
-#         "first_name": "William",
-#         "last_name": "Canin",
-#         "email": "william.costa.canin_x@gmail.com",
-#         "password1": "!Test123",
-#         "password2": "!Test123",
-#     }
-#     user1 = UserRegisterForm(data)
-#     user1.save()
-
-#     c = Client()
-#     response = c.post(
-#         reverse_lazy("sign-in"),
-#         {"username": data["username"], "password": data["password1"]},
-#     )
-#     assert response.url == "/"
-#     assert response.status_code == 302
+    c = Client()
+    response = c.post(
+        reverse_lazy("sign-in"),
+        {"username": data["username"], "password": data["password1"]},
+    )
+    assert response.status_code == 302
 
 
 # @pytest.mark.django_db
@@ -53,8 +51,8 @@
 #         "password1": "!Test123",
 #         "password2": "!Test123",
 #     }
-#     UserRegisterForm(user1).save()
-#     get_user = User.objects.get(username=user1["username"])
+#     UserCreationForm(user1).save()
+#     get_user = UserCustom.objects.get(username=user1["username"])
 #     assert get_user.has_perm("clients.view_clientmodel") is True
 #     with pytest.raises(ValueError):
-#         UserRegisterForm(user2).save()
+#         UserCreationForm(user2).save()
